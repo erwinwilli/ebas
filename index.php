41a8385d1b012e69aab4d6152bfbf2f0114024ff
Nergal88 <vongellhorn@gmx.net>
2014-11-18 14:19:14 +0100

    Index Table + CSS
    Index.php wurde table durch div ersetzt. CSS wurde angepasst

--- index.php
+++ index.php
@@ -11,32 +11,24 @@ require_once 'header.php';
 <div class="row">
         <div class="col-md-12">*suche</div>
 </div>
-<div class="row">
-        <div class="kurs col-md-12">
-          <?php
+<div class="row">	
+    <div class="kurs col-md-12">
+		<?php 
 			$ebas = new ebas;
 			$kurse = $ebas->kurse->getAlleKurse();
-			echo "<table border='3' >";
-			echo "<tr>";
-			echo "<th style=' padding:5px'>Ort</th>";//Titel der Tabele wird eingefügt
-			echo "<th style=' padding:5px'>Sprache</th>"; 
-			echo "<th style=' padding:5px'>Teilnehmer</th>";
-			echo "<tr>";
-			foreach($kurse as $kurs){
-				echo "<tr>";
-				echo "<td style=' padding:5px'>". $kurs["bezeichnung_de"] . "</td>";
-				echo "<td style=' padding:5px'>" .$kurs["sprache"]."</td>";
-				echo "<td style=' padding:5px'>" .$kurs["max_teilnehmer"].'/'.$kurs["max_teilnehmer"]. "</td>";
-				echo "</tr>";
-			}
-			echo "</table>";
-		  
-        
-
-          
-        
-
-          ?>
+			echo '<div class="inner-kurs	 ">';
+				echo '<div class="kurs-ort col-md-7">Kursort und Datum</div>';
+				echo '<div class="kurs-sprache col-md-1">Sprache</div>';
+				echo '<div class="kurs-tz col-md-2">Teilnehmeranzahl</div>';
+				foreach($kurse as $kurs){            
+					echo '<div class="kurs-ort col-md-7">'.$kurs["bezeichnung_de"].'</div>';		
+					echo '<div class="kurs-sprache col-md-1">'.$kurs["sprache"].'</div>';
+					echo '<div class="kurs-tz col-md-2">'.$kurs["max_teilnehmer"].'/'.$kurs["max_teilnehmer"].'</div>';
+				}
+				echo '<div class="kurs-ende col-md-10"></div>';
+			echo '</div>';
+			
+        ?>
         </div>
 </div>
 <table style="margin:50px">
