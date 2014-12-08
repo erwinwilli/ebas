0c4c0fa82e663930a45f97273baef1be11574632
erwinwilli <erwinwilli@windowslive.com>
2014-12-08 17:46:22 +0100

    anpassen berechtigungen

--- Web/Anmeldungen/neuanmeldung.php
+++ Web/Anmeldungen/neuanmeldung.php
@@ -7,7 +7,7 @@ require_once '../../ebas.class.php';
 require_once '../../session.php';
 
 //check Role
-if($ebas->user->role >= 1){
+if($ebas->user->role > 0){
   header('Location: ' .$loginUrl);
 }
 
